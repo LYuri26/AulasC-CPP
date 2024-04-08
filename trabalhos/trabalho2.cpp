22fa3c82be600504274a8f51af7de49423b1ba0d
Lenon Yuri <lenonyurisilva26@gmail.com>
2024-04-08 17:37:27 -0300

    08/04/24

--- trabalhos/trabalho2.cpp
+++ trabalhos/trabalho2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 // Função para calcular o fatorial de um número
-int fatorial(int totalElementos)
+int Fatorial(int totalElementos)
 {
     int resultadoFatorial = 1; // Inicializa o resultado do fatorial como 1
 
@@ -16,38 +16,58 @@ int fatorial(int totalElementos)
 }
 
 // Função para calcular o arranjo simples
-int arranjo(int totalElementos, int elementosEscolhidos)
+int ArranjoSimples(int totalElementos, int elementosEscolhidos)
 {
-    // Calcula o numerador do arranjo
-    int numerador = fatorial(totalElementos);
-    // Calcula o denominador do arranjo
-    int denominador = fatorial(totalElementos - elementosEscolhidos);
-    // Calcula o arranjo
-    int resultadoArranjo = numerador / denominador;
-    return resultadoArranjo;
+    // Calcula o numerador do arranjoSimples
+    int numerador = Fatorial(totalElementos);
+    // Calcula o denominador do arranjoSimples
+    int denominador = Fatorial(totalElementos - elementosEscolhidos);
+    // Calcula o arranjoSimples
+    int resultadoArranjoSimples = numerador / denominador;
+    return resultadoArranjoSimples;
+}
+
+// Função para calcular a combinação simples
+int CombinacaoSimples(int totalElementos, int subConjuntoDoTotalElementos)
+{
+    // Calcula o numerador da combinaçãoSimples
+    int numerador = Fatorial(totalElementos);
+    // Calcula o denominador da combinaçãoSimples
+    int denominador = Fatorial(subConjuntoDoTotalElementos) * Fatorial(totalElementos - subConjuntoDoTotalElementos);
+    // Calcula a combinaçãoSimples
+    int resultadoCombinacaoSimples = numerador / denominador;
+    return resultadoCombinacaoSimples;
 }
 
 int main()
 {
-    int totalElementos, elementosEscolhidos;
+    int totalElementos, elementosEscolhidos, subConjuntoDoTotalElementos;
 
     // Solicita ao usuário que digite o número total de elementos no conjunto
     std::cout << "Digite o Número Total de Elementos no Conjunto: ";
     std::cin >> totalElementos;
 
-    // Solicita ao usuário que digite o número de elementos a serem escolhidos para o arranjo
-    std::cout << "Digite o número de Elementos a serem Escolhidos para o Arranjo: ";
+    // Solicita ao usuário que digite o número de elementos a serem escolhidos para o arranjo simples
+    std::cout << "Digite o Número de Elementos a Serem Escolhidos para o Arranjo: ";
     std::cin >> elementosEscolhidos;
 
+    // Solicita ao usuário que digite o número de elementos a serem escolhidos para o subconjunto
+    std::cout << "Digite o Número de Elementos a Serem Escolhidos para o Subconjunto: ";
+    std::cin >> subConjuntoDoTotalElementos;
+
     // Calcula o fatorial do número total de elementos
-    int fatorialTotalElementos = fatorial(totalElementos);
+    int fatorialTotalElementos = Fatorial(totalElementos);
     // Calcula o arranjo simples
-    int arranjoSimples = arranjo(totalElementos, elementosEscolhidos);
+    int arranjoSimples = ArranjoSimples(totalElementos, elementosEscolhidos);
+    // Calcula a combinação simples
+    int combinacaoSimples = CombinacaoSimples(totalElementos, subConjuntoDoTotalElementos);
 
     // Exibe o resultado do fatorial
-    std::cout << "O fatorial de " << totalElementos << " é: " << fatorialTotalElementos << std::endl;
+    std::cout << "O Fatorial de " << totalElementos << " é: " << fatorialTotalElementos << std::endl;
     // Exibe o resultado do arranjo simples
-    std::cout << "O arranjo simples de " << totalElementos << " elementos escolhendo " << elementosEscolhidos << " é: " << arranjoSimples << std::endl;
+    std::cout << "O Arranjo Simples de " << totalElementos << " Elementos Escolhidos " << elementosEscolhidos << " é: " << arranjoSimples << std::endl;
+    // Exibe o resultado da combinação simples
+    std::cout << "A Combinação Simples de " << totalElementos << " Elementos Escolhidos " << subConjuntoDoTotalElementos << " é: " << combinacaoSimples << std::endl;
 
     return 0;
 }
