c85df941422ffe3e1ad45c2eae8ba178b6635200
Lenon Yuri <lenonyurisilva26@gmail.com>
2024-07-03 21:25:26 -0300

    03/07/24

--- exercicios/Lista 4/agendadecontatos.cpp
+++ exercicios/Lista 4/agendadecontatos.cpp
@@ -15,9 +15,17 @@ int main()
     int num_contatos = 0;         // Contador para controlar o número de contatos cadastrados
 
     // Adiciona alguns contatos à agenda inicialmente
-    agenda[num_contatos++] = {"Coringa", "999-8888"};
-    agenda[num_contatos++] = {"Pinguim", "777-6666"};
-    agenda[num_contatos++] = {"Duas Caras", "555-4444"};
+    Contato contato1 = {"Coringa", "999-8888"};
+    agenda[num_contatos] = contato1;
+    num_contatos = num_contatos + 1; // Incrementa manualmente o contador
+
+    Contato contato2 = {"Pinguim", "777-6666"};
+    agenda[num_contatos] = contato2;
+    num_contatos = num_contatos + 1; // Incrementa manualmente o contador
+
+    Contato contato3 = {"Duas Caras", "555-4444"};
+    agenda[num_contatos] = contato3;
+    num_contatos = num_contatos + 1; // Incrementa manualmente o contador
 
     // Exibe a agenda de contatos inicial
     cout << "Lista de contatos:\n";
