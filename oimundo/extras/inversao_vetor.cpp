d4da5fe642568b61f9e7b817031670cd29c4dcf0
Lenon Yuri <lenonyurisilva26@gmail.com>
2024-07-11 20:57:15 -0300

    11/07

--- oimundo/extras/inversao_vetor.cpp
+++ oimundo/extras/inversao_vetor.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     int vetor[4];
     int temp;
 
     std::cout << "Digite os elementos do vetor (4 elementos): ";
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         std::cin >> vetor[i];
     }
 
-    for (int i = 0; i < 4 / 2; i++) {
-        temp = vetor[i];
-        vetor[i] = vetor[4 - 1 - i];
-        vetor[4 - 1 - i] = temp;
+    // Inversão do vetor começando de i = 4 e subtraindo
+    for (int i = 4; i > 4 / 2; i--)
+    {
+        temp = vetor[4 - i];
+        vetor[4 - i] = vetor[i - 1];
+        vetor[i - 1] = temp;
     }
 
     std::cout << "Vetor invertido: ";
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         std::cout << vetor[i] << " ";
     }
 
